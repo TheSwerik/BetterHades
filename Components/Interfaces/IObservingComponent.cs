d8ba64940a977df4f4fa1b73097567842031db56
TheSwerik <derswerik@gmail.com>
2020-06-16 12:16:29 +0200

    experiment

--- Components/Interfaces/IObservingComponent.cs
+++ Components/Interfaces/IObservingComponent.cs
@@ -5,9 +5,12 @@ namespace BetterHades.Components
 {
     public interface IObservingComponent : IComponent, IObserver<Connection>
     {
+        // Implemented:
         void IObserver<Connection>.OnCompleted() { throw new CompletedException(); }
         void IObserver<Connection>.OnError(Exception error) { Console.WriteLine(error); }
         void IObserver<Connection>.OnNext(Connection value) { Update(value); }
+
+        // Abstract:
         public void Update(Connection value);
     }
 }
