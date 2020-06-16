34a40b4334bdec92826a342998a7ace389fdda8e
TheSwerik <derswerik@gmail.com>
2020-06-16 14:06:44 +0200

    add Or and XOr

---
+++ Components/Implementations/Gates/OrGate.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace BetterHades.Components.Implementations.Gates
+{
+    public class OrGate : Gate
+    {
+        protected override bool Check() { return Inputs.Any(c => c.IsActive); }
+    }
+}
---
+++ Components/Implementations/Gates/XOrGate.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace BetterHades.Components.Implementations.Gates
+{
+    public class XOrGate : Gate
+    {
+        protected override bool Check() { return Inputs.Count(c => c.IsActive) == 1; }
+    }
+}
