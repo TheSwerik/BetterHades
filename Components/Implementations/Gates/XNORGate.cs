3216799ff8b3beb0a6d04927e479647e29739af0
TheSwerik <derswerik@gmail.com>
2020-06-16 14:25:07 +0200

    Add XNOR

---
+++ Components/Implementations/Gates/XNORGate.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable InconsistentNaming
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BetterHades.Components.Implementations.Gates
+{
+    public class XNORGate : Gate
+    {
+        protected override bool Check() { return Inputs.All(c => c == Inputs[0].IsActive); }
+    }
+}
