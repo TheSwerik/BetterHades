c92f841cb6426bde7f6388e036fb614807a21a6c
TheSwerik <derswerik@gmail.com>
2020-06-16 14:35:36 +0200

    add InputClock.cs

---
+++ Components/Implementations/IO/InputClock.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using Avalonia.Controls;
+
+namespace BetterHades.Components.Implementations.IO
+{
+    public class InputClock : Component
+    {
+        public readonly CheckBox InputBox;
+        private readonly long _ms;
+
+        public InputClock(CheckBox inputBox, long ms)
+        {
+            InputBox = inputBox;
+            _ms = ms;
+            new Thread(() =>
+                       {
+                           var stopwatch = new Stopwatch();
+                           while (stopwatch.ElapsedMilliseconds < 10_000)
+                           {
+                               if (stopwatch.ElapsedMilliseconds % _ms < 10) InputBox.IsChecked = !InputBox.IsChecked;
+                           }
+                       }).Start();
+        }
+
+        public void Update()
+        {
+            if (InputBox.IsChecked != null) Notify(IsActive = (bool) InputBox.IsChecked);
+        }
+    }
+}
